27ac966ce49c8dd4dfead5b385f615a303923655
lakith <lakith1995@gmail.com>
2018-06-21 11:04:31 +0530

    first commit

---
+++ src/main/java/com/advance_jersey_app_3/app/jdbcConnection/Resources/FileUploading.java
@@ -0,0 +1,45 @@
+package com.advance_jersey_app_3.app.jdbcConnection.Resources;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.*;
+@Path("/upload")
+public class FileUploading {
+
+    @POST
+    @Path("/pdf")
+    @Consumes(value = {MediaType.MULTIPART_FORM_DATA})
+    public Response pdfFileUpload(@FormDataParam("file")InputStream fileInputStream,
+                                  @FormDataParam("file")FormDataContentDisposition fileMetaData) throws Exception {
+
+        String UPLOAD_PATH = "c:/temp/";
+        try
+        {
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + fileMetaData.getFileName()));
+            while ((read = fileInputStream.read(bytes)) != -1)
+            {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e)
+        {
+            throw new WebApplicationException("Error while uploading file. Please try again !!");
+        }
+        return Response.ok("Data uploaded successfully !!").build();
+
+
+
+    }
+
+}
